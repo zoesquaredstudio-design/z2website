21767056f89d747df65ef5de64c61f489ba38eef
zoe adler <zoeadler@zoes-MacBook-Pro.local>
2026-04-12 23:48:19 -0500

    ball

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
-// Click and drag the mouse to view the scene from different angles.
+// Declare variable for the horizontal position of the circle
+let x = 25;
 
 function setup() {
-  createCanvas(100, 100, WEBGL);
+  // Create the canvas
+  createCanvas(720, 400);
 
-  describe('A white sphere on a gray background.');
+  // Set the color mode to hue-saturation-brightness (HSB)
+  colorMode(HSB);
+
+  // Set the text size
+  textSize(20);
+
+  // No animation to start
+  noLoop();
 }
 
 function draw() {
-  background(150);
+  // Clear the background
+  background(0);
 
-  // Enable orbiting with the mouse.
-  orbitControl();
+  // Draw a circle, with hue determined by frameCount
+  fill(x / 3, 90, 90);
+  circle(x, height / 2, 50);
 
-  // Draw the ellipsoid.
-  // Set its radiusX to 30.
-  ellipsoid(30);
+  // Increase the x variable by 5
+  x += 5;
+
+  // Reset the circle position after it moves off the right side
+  if (x > width + 25) {
+    x = -25;
+  }
+
+  describe('circle moving to the right');
 }
 
+function mousePressed() {
+  // Start/stop the animation loop
+  if (isLooping()) {
+    noLoop();
+  } else {
+    loop();
+  }
+}
+
+function keyPressed() {
+  // Draw one frame
+  redraw();
+}
